86401cb2b3825d4ae8d9edbbcc96167d00693bb7
StacheGamer1609 <72608929+StacheGamer1609@users.noreply.github.com>
2021-05-26 18:22:51 +0200

    Add files via upload

---
+++ src/main/java/com/stachegamer1609/createfa/core/objects/items/MixedEggItem.java
@@ -0,0 +1,24 @@
+package com.stachegamer1609.createfa.core.objects.items;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class MixedEggItem extends Item {
+
+    public MixedEggItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(final ItemStack stack, @Nullable final World worldIn, final List<ITextComponent> tooltip, final ITooltipFlag flagIn) {
+        tooltip.add(new TranslationTextComponent("tooltip.mixed_egg.createfa"));
+    }
+}
+
